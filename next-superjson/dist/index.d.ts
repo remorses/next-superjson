96bd8ae25ad87bf821d1457c5e482b2ebb89ff7f
remorses <beats.by.morse@gmail.com>
2021-10-27 12:39:36 +0200

    rm files, add stuff

--- next-superjson/dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { NextConfig } from 'next';
-export interface NextSuperjsonConfig {
-}
-export declare function withSuperjson(withSuperjson?: NextSuperjsonConfig): (nextConfig?: NextConfig) => NextConfig;
-//# sourceMappingURL=index.d.ts.map
